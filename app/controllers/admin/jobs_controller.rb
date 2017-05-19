c87a72a53dd29b067846f4f5115ffac0330a4be9
Zola Chen <zolacheng@gmail.com>
2017-05-19 16:20:33 +0800

    implement step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -18,10 +18,6 @@ class Admin::JobsController < ApplicationController
     end
   end
 
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
-  end
-
   def new
     @job = Job.new
   end
@@ -57,9 +53,28 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def publish
+   @job = Job.find(params[:id])
+   @job.publish!
+
+   redirect_to :back
+ end
+
+ def hide
+   @job = Job.find(params[:id])
+
+   @job.hide!
+
+   redirect_to :back
+ end
+
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
+
+
+
+
 end
